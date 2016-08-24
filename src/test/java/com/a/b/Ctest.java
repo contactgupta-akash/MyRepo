93c017510ca3e44d8f71f4a9ce807c5ace5c5757
Akash <contactgupta.akash@gmail.com>
2016-08-24 12:31:54 +0530

    firstcommit

---
+++ src/test/java/com/a/b/Ctest.java
@@ -0,0 +1,16 @@
+package com.a.b;
+
+import org.testng.annotations.Test;
+
+public class Ctest {
+	
+	@Test
+	public void test1(){
+	System.out.println("Hi I am test1");	
+	}
+
+	@Test
+	public void test2(){
+		System.out.println("Hi I am test2");
+	}
+}
